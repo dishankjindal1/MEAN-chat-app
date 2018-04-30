6dec76f6e809907424bd1a6a1ecd5993494bb987
Dishank Jindal <dishankjindalitn@gmail.com>
2018-04-30 13:22:06 +0530

    handling middleware

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.use((req, res, next) => {
     console.log('api called by ' + req.ip);
+    next();
 });
 router.get('/logindata', (req, res) => {
     console.log('data : ', req.body);
