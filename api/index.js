20de9f4a1ebdc80e2a0de603c06320212c478c23
Dishank Jindal <dishankjindalitn@gmail.com>
2018-04-30 13:26:27 +0530

    changing router protocol to post

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ router.use((req, res, next) => {
     console.log('api called by ' + req.ip);
     next();
 });
-router.get('/logindata', (req, res) => {
+router.post('/logindata', (req, res) => {
     console.log('data : ', req.body);
     res.send('received!!');
 });
